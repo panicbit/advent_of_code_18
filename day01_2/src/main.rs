8a39fb771ef2f4abcea377f7eda826775cb2cf86
panicbit <panicbit.dev@gmail.com>
2018-11-20 11:40:21 +0100

    Init

---
+++ day01_2/src/main.rs
@@ -0,0 +1,6 @@
+#[macro_use] extern crate aoc;
+
+#[aoc(2018, 01, 2)]
+fn main(input: &str) -> i32 {
+    unimplemented!()
+}
---
+++ day03/src/main.rs
@@ -0,0 +1,6 @@
+#[macro_use] extern crate aoc;
+
+#[aoc(2018, 03, 1)]
+fn main(input: &str) -> i32 {
+    unimplemented!()
+}
