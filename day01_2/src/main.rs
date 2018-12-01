d29cab191f158c409d91621c8b5ac25065f9c800
panicbit <panicbit.dev@gmail.com>
2018-12-01 18:45:40 +0100

    Day 01 part 2

--- day01_2/src/main.rs
+++ day01_2/src/main.rs
@@ -1,6 +1,27 @@
 #[macro_use] extern crate aoc;
 
+use std::collections::HashSet;
+
 #[aoc(2018, 01, 2)]
 fn main(input: &str) -> i32 {
-    unimplemented!()
+    let mut current_freq = 0;
+    let mut freqs = HashSet::new();
+    let changes = input
+        .split_whitespace()
+        .map(|freq| freq.parse::<i32>().unwrap())
+        .cycle();
+
+    freqs.insert(0);
+
+    for change in changes {
+        current_freq += change;
+
+        if freqs.contains(&current_freq) {
+            return current_freq;
+        }
+
+        freqs.insert(current_freq);
+    }
+
+    unreachable!()
 }
