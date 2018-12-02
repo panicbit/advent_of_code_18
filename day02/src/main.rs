618db609ec64541b992e2c859b9baa6ba19c95c5
panicbit <panicbit.dev@gmail.com>
2018-12-02 11:28:37 +0100

    Day 02

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,6 +1,30 @@
 #[macro_use] extern crate aoc;
+extern crate itertools;
+
+use itertools::Itertools;
 
 #[aoc(2018, 02, 1)]
-fn main(input: &str) -> i32 {
-    unimplemented!()
+fn main(input: &str) -> usize {
+    let words = input
+        .split_whitespace();
+
+    let num_two_dupes = words
+        .clone()
+        .filter(|word| has_exact_num_of_dupes(word, 2))
+        .count();
+
+    let num_three_dupes = words
+        .filter(|word| has_exact_num_of_dupes(word, 3))
+        .count();
+
+    num_two_dupes * num_three_dupes
+}
+
+fn has_exact_num_of_dupes(word: &str, num: usize) -> bool {
+    word.chars()
+        .sorted()
+        .into_iter()
+        .group_by(|&c| c)
+        .into_iter()
+        .any(|(_, dupes)| dupes.count() == num)
 }
